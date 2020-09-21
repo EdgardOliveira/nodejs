6b35427a8b3605591894a74e77082657ed6f8eae
Edgard Oliveira <eafo.hef2@uea.edu.br>
2020-09-21 15:33:17 -0400

    add:Implementação de rotas de mensagens

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//Listar todos
+app.get('/mensagens', (req, res) => {
+  res.send('Exibir toda a lista de mensagens')
+})
+
+//Cadastrar
+app.post('/mensagens', (req, res) => {
+    res.send('Criar uma mensagem')
+})
+
+//Listar um especifico
+app.get('/mensagens/:id', (req, res) => {
+    res.send('Listar uma mensagem especifica')
+})
+
+//Atualizar uma mensagem especifica
+app.put('/mensagens/:id', (req, res) => {
+    res.send('Atualizar uma mensagem')
+})
+
+//Excluir uma mensagem específica
+app.delete('/mensagens/:id', (req, res) => {
+    res.send('Excluir uma mensagem')
 })
 
 app.listen(port, () => {
