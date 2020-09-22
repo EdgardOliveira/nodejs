22ff71710031207b839a296dcd0b0bbdf9eb7260
Edgard Oliveira <eafo.hef2@uea.edu.br>
2020-09-22 15:32:19 -0400

    aula 2

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const jsonParser = bodyParser.json();
+app.use(jsonParser)
+
+const mensagens = [
+    {
+        id : 0,
+        texto : "essa é uma mensagem",
+    },
+    {
+        id : 1,
+        texto: "essa é outra mensagem"
+    },
+];
+
 //Listar todos
 app.get('/mensagens', (req, res) => {
-  res.send('Exibir toda a lista de mensagens')
+  res.json(mensagens.filter(Boolean))
 })
 
 //Cadastrar
 app.post('/mensagens', (req, res) => {
-    res.send('Criar uma mensagem')
+    const mensagem = req.body.mensagem
+
+    const id = mensagens.length
+    
+    mensagem.id = id
+    
+    mensagens.push(mensagem)
+
+    console.log(mensagens)
+
+    res.send(`A mensagem '${mensagem.texto}' foi criada com sucesso. Id ${mensagem.id}` )
 })
 
 //Listar um especifico
 app.get('/mensagens/:id', (req, res) => {
-    res.send('Listar uma mensagem especifica')
+    const id = req.params.id
+    
+    console.log(id)
+
+    const mensagem = mensagens[id]
+
+    res.send(`Listar uma mensagem especifica com o id ${id} '${mensagem}'`)
 })
 
 //Atualizar uma mensagem especifica
 app.put('/mensagens/:id', (req, res) => {
-    res.send('Atualizar uma mensagem')
+    const id = req.params.id
+
+    const texto = req.body.texto
+
+    mensagens[id].texto = texto
+
+    res.send(`Mensagem com Id ${id} atualizada para '${mensagens[id].texto}'`)
 })
 
 //Excluir uma mensagem específica
 app.delete('/mensagens/:id', (req, res) => {
+    const id = req.params.id
+
+    delete mensagems[id]
+
     res.send('Excluir uma mensagem')
 })
 
